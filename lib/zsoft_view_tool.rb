a7d8a18c53164cf6f39b0f1dec731df7678fdf09
Mike Zanchettin <mike.zanchettin@gmail.com>
2018-08-11 19:03:42 -0500

    Initial commit

---
+++ lib/zsoft_view_tool.rb
@@ -0,0 +1,6 @@
+require "zsoft_view_tool/version"
+require "zsoft_view_tool/renderer"
+
+module ZsoftViewTool
+  # Your code goes here...
+end
---
+++ lib/zsoft_view_tool/version.rb
@@ -0,0 +1,3 @@
+module ZsoftViewTool
+  VERSION = "0.1.0"
+end
